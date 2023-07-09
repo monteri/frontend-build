076b78b18fffae9d2b38a788fdbabea5da07a2ed
monteri <lansevermore>
2023-07-09 19:57:40 +0300

    feat: copying

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -1,4 +1,36 @@
+const fs = require('fs');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+function getOverrides() {
+  const currentMfe = path.basename(process.cwd())
+  const overridesPath = path.resolve(__dirname, `../overrides/${currentMfe}`);
+  const overrides = [];
+
+  function processDirectory(directory) {
+    const files = fs.readdirSync(directory);
+
+    files.forEach((file) => {
+      const filePath = path.join(directory, file);
+      const stats = fs.statSync(filePath);
+
+      if (stats.isFile()) {
+        const relativePath = path.relative(overridesPath, filePath);
+        const originalPath = path.join(process.cwd(), relativePath);
+        overrides.push({
+          from: filePath,
+          to: originalPath,
+        })
+      } else if (stats.isDirectory()) {
+        processDirectory(filePath); // Recursively process subdirectories
+      }
+    });
+  }
+
+  processDirectory(overridesPath);
+  console.log('overrides', overrides);
+  return overrides;
+}
 
 module.exports = {
   entry: {
@@ -11,6 +43,7 @@ module.exports = {
   resolve: {
     alias: {
       'env.config': path.resolve(process.cwd(), './env.config'),
+      ...getOverrides(),
     },
     fallback: {
       // This causes the system to return an empty object if it can't find an env.config.js file in
@@ -19,4 +52,11 @@ module.exports = {
     },
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        ...getOverrides()
+      ],
+    }),
+  ],
 };
